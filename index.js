9cfa7bf2546824bd4e89e9946a214c5f7fd2071d
DahisC <dahischeng@gmail.com>
2022-01-15 13:28:32 +0800

    55

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 const fs = require('fs');
+const axios = require('axios');
+
+const sheetId = '12Da53Wv7IpJCkC5xjgQAnJGZ9451T6Tf76mkhurSBsc';
+
+const gid = {
+  Common: 0,
+  Exception: 872461849,
+  Error: 1716244250,
+  Flagmap: 878857132,
+  Admin: 1723346815,
+  Dealer: 126890182,
+  Tablet: 1403093468,
+};
+
+async function download() {
+  const res = await axios.get(
+    'https://docs.google.com/spreadsheets/d/12Da53Wv7IpJCkC5xjgQAnJGZ9451T6Tf76mkhurSBsc/gviz/tq?tqx=out:csv&gid=872461849'
+  );
+  console.log(res);
+  res.data.replace('"",', '');
+  res.data.replace('"', '');
+  fs.writeFileSync('test.csv', res.data);
+}
+
+download();
 
 console.log('yoyoyo');
